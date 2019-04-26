536427ba65d50c32fa8a41590455ced0552c938e
Reza Soltani <rsoltani97@gmail.com>
2019-04-26 17:35:12 +0430

    Add menu to our new custom toolbar

--- app/src/main/java/com/example/proj2/MainActivity.java
+++ app/src/main/java/com/example/proj2/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.proj2;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,17 +27,23 @@ public class MainActivity extends AppCompatActivity {
     private List<Post> postList;
     private DataRepository dataRepository;
     private CompositeDisposable compositeDisposable;
+    private Menu toolbarMenu;
+    private RecyclerView.ItemDecoration horizontalDecoration;
+    private boolean isGrid = false;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        myToolbar.setBackgroundColor(Color.parseColor("#EF9A9A"));
+        setSupportActionBar(myToolbar);
 
         dataRepository = new DataRepository(this);
 
         postList = new ArrayList<>();
-        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView = findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.addItemDecoration(new DividerItemDecoration(this,
                 DividerItemDecoration.VERTICAL));
@@ -52,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Consumer<List<Post>>() {
                     @Override
-                    public void accept(List<Post> posts) throws Exception {
+                    public void accept(List<Post> posts) {
                         postList.clear();
                         postList.addAll(posts);
                         adapter.notifyDataSetChanged();
@@ -67,5 +78,34 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
 
         compositeDisposable.dispose();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        toolbarMenu = menu;
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_grid:
+                if (!isGrid) {
+                    isGrid = true;
+                    mRecyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
+                    horizontalDecoration = new DividerItemDecoration(this,
+                            DividerItemDecoration.HORIZONTAL);
+                    mRecyclerView.addItemDecoration(horizontalDecoration);
+
+                    toolbarMenu.getItem(0).setIcon(R.drawable.ic_action_non_grid);
+                } else {
+                    isGrid = false;
+                    mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+                    mRecyclerView.removeItemDecoration(horizontalDecoration);
+                    toolbarMenu.getItem(0).setIcon(R.drawable.ic_action_grid);
+
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
